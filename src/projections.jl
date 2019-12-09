38521944933c11de41b70eeddacaded4757e7b1d
Vaclav Macha <vaclav.macha92@gmail.com>
2019-12-09 11:47:41 +0100

    Added general solver for projections.

--- src/projections.jl
+++ src/projections.jl
@@ -1,6 +1,34 @@
 # -------------------------------------------------------------------------------
 # Projection PatMat hinge
 # -------------------------------------------------------------------------------
+# general solver
+function projection_exact(α0::AbstractVector,
+                          β0::AbstractVector,
+                          δ0::Real,
+                          C1::Real,
+                          C2::Real;
+                          solver::Any = ECOS.ECOSSolver(verbose = false))
+
+    α = Convex.Variable(length(α0))
+    β = Convex.Variable(length(β0))
+    δ = Convex.Variable()
+
+    objective   = Convex.sumsquares(α - α0)/2 + Convex.sumsquares(β - β0)/2 +
+                  Convex.sumsquares(δ - δ0)/2
+    constraints = [Convex.sum(α) == Convex.sum(β),
+                   α <= C1,
+                   α >= 0,
+                   β <= C2*δ,
+                   β >= 0]
+
+    problem = Convex.minimize(objective, constraints)
+    Convex.solve!(problem, solver)
+
+    return vec(α.value), vec(β.value), δ.value
+end
+
+
+# our solution
 function f(μ::Real, α0::AbstractVector, β0::AbstractVector, δ0::Real, C1::Real, C2::Real)
     return C2*δ0 + C2^2*sum(max.(β0 .- μ, 0)) - μ
 end
@@ -20,8 +48,8 @@ function projection(α0::AbstractVector, β0::AbstractVector, δ0::Real, C1::Rea
 
     μ_ub1 = minimum(β0)
     μ_ub2 = minimum(C2*δ0 + C2^2*sum(β0) .- α0)/(C2^2*length(β0) + 1)
-    μ_ub = min(μ_ub1, μ_ub2) - 1e8
-    μ_lb = maximum(β0) + C2*max(δ0, 0) + 1e8
+    μ_ub  = min(μ_ub1, μ_ub2) - 1e8
+    μ_lb  = maximum(β0) + C2*max(δ0, 0) + 1e8
 
     μ = Roots.find_zero(μ -> h(μ, α0, β0, δ0, C1, C2), (μ_lb, μ_ub))
     λ = f(μ, α0, β0, δ0, C1, C2)
@@ -37,6 +65,31 @@ end
 # -------------------------------------------------------------------------------
 # Projection PatMat quadratic
 # -------------------------------------------------------------------------------
+# general solver
+function projection_exact(α0::AbstractVector,
+                          β0::AbstractVector,
+                          δ0::Real;
+                          solver::Any = ECOS.ECOSSolver(verbose = false))
+
+    α = Convex.Variable(length(α0))
+    β = Convex.Variable(length(β0))
+    δ = Convex.Variable()
+
+    objective   = Convex.sumsquares(α - α0)/2 + Convex.sumsquares(β - β0)/2 +
+                  Convex.sumsquares(δ - δ0)/2
+    constraints = [Convex.sum(α) == Convex.sum(β),
+                   α >= 0,
+                   β >= 0,
+                   δ >= 0]
+
+    problem = Convex.minimize(objective, constraints)
+    Convex.solve!(problem, solver)
+
+    return vec(α.value), vec(β.value), δ.value
+end
+
+
+# our solution
 function f(λ::Real, α0::AbstractVector, β0::AbstractVector, δ0::Real)
     return sum(max.(α0 .- λ, 0)) - sum(max.(β0 .+ λ, 0))
 end
@@ -61,6 +114,31 @@ end
 # -------------------------------------------------------------------------------
 # Projection TopPushK hinge
 # -------------------------------------------------------------------------------
+# general solver
+function projection_exact(α0::AbstractVector,
+                          β0::AbstractVector,
+                          C::Real,
+                          K::Integer;
+                          solver::Any = ECOS.ECOSSolver(verbose = false))
+
+    α = Convex.Variable(length(α0))
+    β = Convex.Variable(length(β0))
+
+    objective   = Convex.sumsquares(α - α0)/2 + Convex.sumsquares(β - β0)/2
+    constraints = [Convex.sum(α) == Convex.sum(β),
+                   α <= C,
+                   α >= 0,
+                   β <= Convex.sum(α)/K,
+                   β >= 0]
+
+    problem = Convex.minimize(objective, constraints)
+    Convex.solve!(problem, solver)
+
+    return vec(α.value), vec(β.value)
+end
+
+
+# our solution
 function f(μ::Real, s::AbstractVector, K::Integer)
     i, j, d  = 2, 1, 1
     λ, λ_old = s[1], 0
@@ -121,6 +199,29 @@ end
 # ---------------------------------------------------------------------------------------
 # Projection TopPushK quadratic
 # ---------------------------------------------------------------------------------------
+# general solver
+function projection_exact(α0::AbstractVector,
+                          β0::AbstractVector,
+                          K::Integer;
+                          solver::Any = ECOS.ECOSSolver(verbose = false))
+
+    α = Convex.Variable(length(α0))
+    β = Convex.Variable(length(β0))
+
+    objective   = Convex.sumsquares(α - α0)/2 + Convex.sumsquares(β - β0)/2
+    constraints = [Convex.sum(α) == Convex.sum(β),
+                   α >= 0,
+                   β <= Convex.sum(α)/K,
+                   β >= 0]
+
+    problem = Convex.minimize(objective, constraints)
+    Convex.solve!(problem, solver)
+
+    return vec(α.value), vec(β.value)
+end
+
+
+# our solution
 function h(μ::Real, s::AbstractVector, α0::AbstractVector, β0::AbstractVector, K::Integer)
     λ = f(μ, s, K)
     return sum(max.(α0 .- λ .+ sum(max.(β0 .+ λ .- μ, 0))/K, 0)) - K*μ
